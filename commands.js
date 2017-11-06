6c5872b81c9cdb823dc547cda0ed3d8184045c40
Sam Chai <sam.chai0501@gmail.com>
2017-11-06 14:26:16 -0500

    added head and tail functionality

--- commands.js
+++ commands.js
@@ -1,49 +1,53 @@
+'use strict';
+
+var fs = require('fs');
+
 module.exports = {
-    pwd: function() {
+    pwd: function () {
         process.stdout.write(process.execPath);
         process.stdout.write('\nprompt > ');
     },
-    date: function() {
-        let date = new Date;
+    date: function () {
+        let date = new Date();
         process.stdout.write(date.toString());
         process.stdout.write('\nprompt > ');
     },
-    ls: function() {
-        var fs = require('fs');
-        fs.readdir('.', function(err, files) {
+    ls: function () {
+        fs.readdir('.', function (err, files) {
             if (err) throw err;
-            files.forEach(function(file) {
+            files.forEach(function (file) {
                 process.stdout.write(file.toString() + "\n");
             })
             process.stdout.write("prompt > ");
         });
     },
-    echo: function(args) {
-        if(args[0] === '$') {
+    echo: function (args) {
+        if (args[0] === '$') {
             process.stdout.write(process.env[args.slice(1)]);
         } else {
             process.stdout.write(args);
         }
         process.stdout.write('\nprompt > ');
     },
-    cat: function(file) {
-        var fs = require('fs');
-        fs.readFile('./' + file, (err,data) => {
-            if(err) throw err;
+    cat: function (file) {
+        fs.readFile('./' + file, (err, data) => {
+            if (err) throw err;
             process.stdout.write(data);
         });
         process.stdout.write('\nprompt > ');
     },
-    head: function(file) {
-        var fs = require('fs');
-        var lines = 0;
-        fs.open('./' + file, 'r', (err, data) => {
-            if(err || lines === 5) {
-                fs.close(data);
-            }
-            process.stdout.write(data);
-            lines++;
+    head: function (file) {
+        fs.readFile(file, 'utf8', (err, data) => {
+            if (err) throw err;
+            process.stdout.write(data.split('\n').slice(0, 5).join('\n'));
+            process.stdout.write('\nprompt > ');
         });
-        process.stdout.write('\nprompt > ');
+    },
+    tail: function (file) {
+        fs.readFile(file, 'utf8', (err, data) => {
+            if (err) throw err;
+            process.stdout.write(data.split('\n').slice(-5).join('\n'));
+            process.stdout.write('\nprompt > ');
+        });
     }
 };
